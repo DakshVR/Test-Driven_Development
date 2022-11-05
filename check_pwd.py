ec1c96a551983337f8163a54ccd8746f6dd74526
Daksh Viradiya <dakshviradiya@example.com>
2022-11-05 14:54:45 -0700

    Made check_pwd return False when pwd does not contain lower case character

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,13 @@
 def check_pwd(pwd):
     if len(pwd) < 8 or len(pwd) > 20:
         return False
+
+    lower_case = False
+    for character in pwd:
+        if character.islower():
+            lower_case = True
+
+    if lower_case == False:
+        return False
+
     return True
