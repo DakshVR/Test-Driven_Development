bdee6afa3adc9406e8f44c8c84a629ae9681c8bb
Daksh Viradiya <dakshviradiya@example.com>
2022-11-05 15:43:10 -0700

    Made check_pwd return False when password does not contain any symbols, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -2,9 +2,12 @@ def check_pwd(pwd):
     if len(pwd) < 8 or len(pwd) > 20:
         return False
 
+    symbols = '~`!@#$%^&*()_+-='
     lower_case = False
     upper_case = False
     digit = False
+    symbol = False
+
     for character in pwd:
         if character.islower():
             lower_case = True
@@ -12,8 +15,10 @@ def check_pwd(pwd):
             upper_case = True
         elif character.isdigit():
             digit = True
+        elif character in symbols:
+            symbol = True
 
-    if (lower_case and upper_case and digit) == False:
+    if (lower_case and upper_case and digit and symbol) == False:
         return False
 
     return True
