23ab0536a7ef40e1989b7e36d05ab5bc95637193
Daksh Viradiya <dakshviradiya@example.com>
2022-11-05 15:22:34 -0700

    Made check_pwd return False when pwd does not contain any Upper case character

--- check_pwd.py
+++ check_pwd.py
@@ -3,11 +3,14 @@ def check_pwd(pwd):
         return False
 
     lower_case = False
+    upper_case = False
     for character in pwd:
         if character.islower():
             lower_case = True
+        elif character.isupper():
+            upper_case = True
 
-    if lower_case == False:
+    if (lower_case and upper_case) == False:
         return False
 
     return True
